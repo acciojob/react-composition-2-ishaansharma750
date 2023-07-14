48278bee20da6ee1a6468d14c80fb82c6970c808
Suansh Budhiraja <43946155+ishaansharma750@users.noreply.github.com>
2023-07-14 12:30:17 +0000

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
+import Modal from "./Model";
 
 const App = () => {
+
+  const [modalShow, setModalShow] = useState(false);
+
+  const handleModalClose = () => {
+    setModalShow(false);
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+        <button className="open-btn" onClick={() => setModalShow(true)}>Show Modal</button>
+        <Modal show={modalShow} onClose={handleModalClose}>
+        <p>This is the content of the modal.</p>
+      </Modal>
     </div>
   )
 }
